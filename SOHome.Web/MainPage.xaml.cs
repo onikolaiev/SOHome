cd9c40620ef6d3394d8209bdb020a6f83421728c
Osni Pezzini Junior <osni.pezzini@gmail.com>
2021-11-17 12:05:51 -0300

    Criado projeto Blazor

---
+++ SOHome.Web/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace SOHome.Web
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
